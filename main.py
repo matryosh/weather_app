fe1f54862518facf2e40dd6653c87294345a6544
matryosh <tbrick1234@gmail.com>
2016-12-10 16:05:23 -0500

    finished Chapter 3: Manipulating Widgets. These were the changes added

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import ObjectProperty
 from kivy.network.urlrequest import UrlRequest
+from kivy.uix.listview import ListItemButton
+from kivy.factory import Factory
 import json
 
 class AddLocationForm(BoxLayout):
@@ -23,7 +25,7 @@ class AddLocationForm(BoxLayout):
 
         #print ("The user searched for '{}'".format(self.search_input.text))
 
-    def get_lat_get_long(self):
+    '''def get_lat_get_long(self):
 
         if self.lat == "" or self.long == "":
             return
@@ -31,7 +33,7 @@ class AddLocationForm(BoxLayout):
         appidkey = "978031dbd4ea52dc90c8dfc5502d536b"
         search_template = "http://api.openweathermap.org/data/2.5/weather?lat={}&lon={}&APPID=" + appidkey
         search_url = search_template.format(self.lat.text,self.long.text)
-        request = UrlRequest(search_url, self.found_lat_and_long)
+        request = UrlRequest(search_url, self.found_lat_and_long)'''
 
         #print(search_url)
 
@@ -39,10 +41,13 @@ class AddLocationForm(BoxLayout):
         data = json.loads(data.decode()) if not isinstance(data, dict) else data
         cities = ["{} ({})".format(d['name'], d['sys']['country'])
                   for d in data['list']]
-        print(cities)
+        #print(cities)
         self.search_results.item_strings = cities
+        del self.search_results.adapter.data[:]
+        self.search_results.adapter.data.extend(cities)
+        self.search_results._trigger_reset_populate()
 
-    def found_lat_and_long(self, request, data):
+    '''def found_lat_and_long(self, request, data):
         arr = []
         data = json.loads(data.decode()) if not isinstance(data, dict) else data
         city = "{} ({})".format(data['name'], data['sys']['country'])
@@ -50,8 +55,21 @@ class AddLocationForm(BoxLayout):
         arr.append(city)
         print(arr)
         self.search_results.item_strings = arr
-        #print data['sys']['country']
+        #print data['sys']['country']'''
 
+class LocationButton(ListItemButton):
+    pass
+
+class WeatherRoot(BoxLayout):
+    def show_current_weather(self, location):
+        self.clear_widgets()
+        current_weather = Factory.CurrentWeather()
+        current_weather.location = location
+        self.add_widget(current_weather)
+
+    def show_add_location_form(self):
+        self.clear_widgets()
+        self.add_widget(AddLocationForm())
 
 class WeatherApp(App):
     pass
